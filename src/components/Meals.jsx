274f7cd882db75d0fe03fcabd9ff56e03a32f8e1
Tafita Hery <tafitahery@gmail.com>
2023-01-10 22:04:28 +0300

    Meals Component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,42 @@
 import React from 'react';
+import { BsHandThumbsUp } from 'react-icons/bs';
+import { useGlobalContext } from '../context';
 
 export default function Meals() {
-  return <div>Meals</div>;
+  const { loading, meals } = useGlobalContext();
+
+  if (loading) {
+    return (
+      <section className="section">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+
+  if (meals.length < 1) {
+    return (
+      <section className="section">
+        <h4>No meals matched your search term. Please try again.</h4>
+      </section>
+    );
+  }
+
+  return (
+    <section className="section-center">
+      {meals.map((singleMeal) => {
+        const { idMeal, strMeal: title, strMealThumb: image } = singleMeal;
+        return (
+          <article key={idMeal} className="single-meal">
+            <img src={image} alt="" className="img" />
+            <footer>
+              <h5>{title}</h5>
+              <button className="like-btn">
+                <BsHandThumbsUp />
+              </button>
+            </footer>
+          </article>
+        );
+      })}
+    </section>
+  );
 }
---
+++ src/context.jsx
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+
+const AppContext = React.createContext();
+
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a';
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+  const [meals, setMeals] = useState([]);
+
+  const fetchMeals = async (url) => {
+    setLoading(true);
+    try {
+      const { data } = await axios.get(url);
+      if (data.meals) {
+        setMeals(data.meals);
+      } else {
+        setMeals([]);
+      }
+    } catch (error) {
+      console.log(error.response);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMeals(allMealsUrl);
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ loading, meals }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
